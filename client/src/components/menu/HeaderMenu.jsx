ecbf22611282097c3018002dc34e147927f8d914
Muhammad Afnan Abbasi <abbasiafnan9@gmail.com>
2021-11-30 14:32:31 -0800

    making search bar

--- client/src/components/menu/HeaderMenu.jsx
+++ client/src/components/menu/HeaderMenu.jsx
@@ -1,15 +1,27 @@
 import { useState, useContext } from 'react';
-import { Menu, MenuItem } from '@material-ui/core';
+import { Menu, MenuItem, makeStyles } from '@material-ui/core';
 import { MoreVert } from '@material-ui/icons';
 import { GoogleLogout } from 'react-google-login';
 import { clientId } from '../../constants/data';
 
 import { AccountContext } from '../../context/AccountProvider';
 
+const useStyles = makeStyles({
+    menuItem: {
+        fontSize: 14,
+        padding: '15px 60px 5px 24px',
+        color: '#4A4A4A'
+    },
+    border: 'none!important',
+    boxShadow: 'none!important'
+
+})
 
 const HeaderMenu = () => {
     const [open, setOpen] = useState(false)
     const { setAccount} = useContext(AccountContext);
+    const classes = useStyles();
+
     const handleClose = () => {
         setOpen(false);
     }
@@ -39,12 +51,13 @@ const HeaderMenu = () => {
                 }}
 
             >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>
+                <MenuItem className={classes.menuItem} onClick={handleClose}>Profile</MenuItem>
+                <MenuItem className={classes.menuItem} onClick={handleClose}>
                     <GoogleLogout
                      clientId={clientId}
                      buttonText="Logout"
                      onLogoutSuccess={onLogoutSuccess}
+                     className={classes.logout}
                      >
 
                     </GoogleLogout >
--- client/src/components/menu/Search.jsx
+++ client/src/components/menu/Search.jsx
@@ -1,10 +1,26 @@
+import { Box, InputBase, makeStyles } from '@material-ui/core';
+import { Search as SearchIcon} from '@material-ui/icons';
 
+const useStyles = makeStyles({
 
-
+})
 
 const Search = ()=> {
+    const classes = useStyles();
     return(
-        <p>Hello</p>
+        <Box className={classes.search}>
+            <Box className={classes.searchIcon}>
+                <SearchIcon />
+            </Box>
+            <InputBase
+            placeholder="Search.."
+            classes={{
+                root:classes.inputRoot,
+                input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search'}}
+            />
+        </Box>
     )
 }
 
