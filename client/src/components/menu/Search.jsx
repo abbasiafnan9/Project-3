39e67d45079a7e5f036831a9811254acc0ddd52e
Muhammad Afnan Abbasi <abbasiafnan9@gmail.com>
2021-12-01 11:01:59 -0800

    styling search bar

--- client/src/components/menu/Search.jsx
+++ client/src/components/menu/Search.jsx
@@ -1,9 +1,40 @@
-import { Box, InputBase, makeStyles } from '@material-ui/core';
+import { alpha, Box, InputBase, makeStyles } from '@material-ui/core';
 import { Search as SearchIcon} from '@material-ui/icons';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme =>({
+    search: {
+        position: 'relative',
+        borderRadius: 18,
+        backgroundColor: '#FFFFFF',
+        marginRight: theme.spacing(2),
+        margin: '0 13px',
+        width: '100%',
+    },
+    searchIcon: {
+        padding: theme.spacing(0,2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        width: '100%'
+    },
+    inputInput: {
+        padding: theme.spacing(1,1,1,0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: '65px',
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
 
-})
+        }
+    }
+
+}))
 
 const Search = ()=> {
     const classes = useStyles();
